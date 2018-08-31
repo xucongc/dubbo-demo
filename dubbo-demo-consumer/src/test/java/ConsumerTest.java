86dc65b9cc42deea57bbcbf2d554279ece336029
xucongcong <xucongcong@yunzhangfang.com>
2018-08-31 15:01:03 +0800

    优化提示信息

--- dubbo-demo-consumer/src/test/java/ConsumerTest.java
+++ dubbo-demo-consumer/src/test/java/ConsumerTest.java
@@ -6,18 +6,21 @@ import com.java.service.DemoProviderService;
 
 public class ConsumerTest {
 
-	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"dubbo-demo-consumer.xml"});
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"dubbo-demo-consumer.xml"});
         context.start();
-        DemoProviderService demoProviderService=(DemoProviderService) context.getBean("demoProviderService");
-        String result=demoProviderService.sayHello("你好");
-        System.out.println("远程调用的结果："+result);
-        try {
-			System.in.read();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        DemoProviderService demoProviderService = (DemoProviderService) context.getBean("demoProviderService");
+        //循环调用远程接口(provider起多个校验负载均衡)
+        for (int i = 0; i < 1000; i++ ){
+            String result=demoProviderService.sayHello("dubbo");
+            System.out.println("远程调用的结果："+result);
+            //线程暂停一秒
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         context.close();
-	}
+    }
 }
--- dubbo-demo-provider/src/main/java/com/java/service/impl/DemoProviderServiceImpl.java
+++ dubbo-demo-provider/src/main/java/com/java/service/impl/DemoProviderServiceImpl.java
@@ -1,22 +1,31 @@
 package com.java.service.impl;
 
+import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.common.utils.NetUtils;
-import com.alibaba.dubbo.registry.dubbo.DubboRegistryFactory;
-import com.alibaba.dubbo.remoting.transport.netty4.logging.NettyHelper;
+import com.alibaba.dubbo.remoting.exchange.ExchangeServer;
 import com.alibaba.dubbo.rpc.protocol.dubbo.DubboProtocol;
-import com.alibaba.dubbo.rpc.protocol.rest.DubboHttpServer;
+import com.alibaba.dubbo.rpc.support.ProtocolUtils;
 import com.java.service.DemoProviderService;
+import java.util.Collection;
 
 /**
  * 服务提供者接口实现类
- * @author Administrator
  *
+ * @author Administrator
  */
-public class DemoProviderServiceImpl implements DemoProviderService{
+public class DemoProviderServiceImpl implements DemoProviderService {
 
-	public String sayHello(String name) {
-		String port ="";
-		return "Hello"+name+"服务端口号："+port;
-	}
+    public String sayHello(String name) {
+        DubboProtocol dubboProtocol = DubboProtocol.getDubboProtocol();
+        Collection<ExchangeServer> servers = dubboProtocol.getServers();
+        int port = 0;
+        if (null != servers) {
+            for (ExchangeServer server : servers) {
+                URL url = server.getUrl();
+                port = url.getPort();
+            }
+        }
+        return "Hello" + name + "服务端口号:"+port;
+    }
 
 }
